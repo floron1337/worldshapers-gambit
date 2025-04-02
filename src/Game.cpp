d7b12a000538ac4a149b25abd07f78d1918bcc3a
Florin Venis <florinvenis@gmail.com>
2025-04-02 18:37:34 +0300

    creat un window pentru joc

---
+++ src/Game.cpp
@@ -0,0 +1,36 @@
+#include "../headers/Game.h"
+#include "../headers/MenuScreen.h"
+
+
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+Game::Game() {
+    this->max_fps = 60;
+}
+
+Game::Game(int max_fps_) {
+    this->max_fps = max_fps_;
+}
+
+Game::~Game() = default;
+
+void Game::start() {
+    // Get the desktop resolution
+    const sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+
+    // Create a window covering the entire screen
+    sf::RenderWindow window(desktop, "Worldshaper's Gambit", sf::Style::Fullscreen);
+    window.setFramerateLimit(this->max_fps);
+
+    MenuScreen menu_screen;
+
+    while (window.isOpen())
+    {
+        window.clear();
+        menu_screen.drawScreen(window);
+        window.display();
+    }
+}
