ddaf6a6bd9173e4b7889d8cfc178e67199582ac5
Florin Venis <florinvenis@gmail.com>
2025-04-15 02:41:30 +0300

    rezolvata eroare compilare cppcheck

--- src/Card.cpp
+++ src/Card.cpp
@@ -5,17 +5,22 @@
 #include <iostream>
 #include "../headers/Card.h"
 
-Card::Card() {
-    card_text = "";
-    card_front_location = "../images/cards/placeholder.png";
-    card_back_location = "../images/cards/back.png";
+Card::Card()
+    : card_text(""),
+      card_front_location("../images/cards/placeholder.png"),
+      card_back_location("../images/cards/back.png")
+{
 }
 
-Card::Card(const Card &other) {
-    card_text = other.card_text;
-    card_front_location = other.card_front_location;
-    card_back_location = other.card_back_location;
+
+Card::Card(const Card &other)
+    : card_text(other.card_text),
+      card_front_location(other.card_front_location),
+      card_back_location(other.card_back_location)
+{
+
 }
+
 
 Card& Card::operator=(const Card &other) {
     if (this == &other) {
@@ -35,11 +40,16 @@ Card::~Card() {
     next_card = nullptr;
 }
 
-Card::Card(const std::string &card_text_, const std::string &card_front_location_, const std::string &card_back_location_) {
-    card_text = card_text_;
-    card_front_location = card_front_location_;
-    card_back_location = card_back_location_;
+Card::Card(const std::string &card_text_,
+           const std::string &card_front_location_,
+           const std::string &card_back_location_)
+    : card_text(card_text_),
+      card_front_location(card_front_location_),
+      card_back_location(card_back_location_)
+{
+
 }
+
 
 std::ostream& operator<<(std::ostream& os, const Card& card) {
     os << "Card Text: " << card.getText() << "\n"
