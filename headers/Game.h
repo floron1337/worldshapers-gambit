ebb7daa61e9260506bc6bc3decbac3f673215c31
Florin Venis <florinvenis@gmail.com>
2025-06-25 17:14:01 +0300

    rezolvat problema singleton

--- headers/Game.h
+++ headers/Game.h
@@ -1,11 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
+#include <iostream>
+
 #include "ScreenManager.h"
 #include "SoundManager.h"
 
 class Game {
     unsigned max_fps = 60;
-    static Game* instance;
 
     sf::RenderWindow window;
     SoundManager sound_manager;
@@ -19,7 +20,7 @@ public:
     ~Game() = default;
     void start();
     //void set_max_fps(unsigned max_fps_) { max_fps = max_fps_; }
-    static Game* getInstance();
+    static Game& getInstance();
 };
 
 #endif //GAME_H
--- src/Game.cpp
+++ src/Game.cpp
@@ -8,17 +8,14 @@
 
 #include "../headers/SoundManager.h"
 
-Game* Game::instance = nullptr;
 
 Game::Game(unsigned max_fps_) : max_fps(max_fps_), window(sf::VideoMode::getDesktopMode(), "Worldshaper's Gambit", sf::Style::Fullscreen),
                            sound_manager(50.0f), screen_manager(window, sound_manager) {
     window.setFramerateLimit(max_fps);
 }
 
-Game* Game::getInstance() {
-    if (instance == nullptr) {
-        instance = new Game();
-    }
+Game& Game::getInstance() {
+    static Game instance;
     return instance;
 }
 
